011ade9c6bbf4a7c4b0e2e9a056f790618a09faa
anuarmurawski84 <edart84@mail.ru>
2021-09-07 03:44:12 +0300

    add test_get_user_details_auth_as_other_user

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -24,7 +24,29 @@ class TestUserGet(BaseCase):
 
         response2 = MyRequests.get(f"/user/{user_id_from_auth_method}",
                                 headers = {'x-csrf-token': token},
-                                cookies = {'auth_sid': auth_sid}
-                                 )
+                                cookies = {'auth_sid': auth_sid})
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+
+    def test_get_user_details_auth_as_other_user(self):
+        data = {
+            "email": "vinkotov@example.com",
+            "password": "1234"
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+        # print("\n")
+        # print(user_id_from_auth_method)
+
+        # Send request with another id
+        response2 = MyRequests.get(url="/user/1",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid})
+        Assertions.assert_json_has_key(response2, "username")
+        expected_fields = ["email", "firstName", "lastName"]
+        Assertions.assert_json_has_not_keys(response2, expected_fields)
